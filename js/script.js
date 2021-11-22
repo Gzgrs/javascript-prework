a45a95df2fd663e138908d7dc6ea1327e9819a13
Grzegorz Siudak <siudak.grzegorz@gmail.com>
2021-11-22 16:01:49 +0000

    Add script for computer move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-let playerMove= `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagralem ' + playerMove + '! Jesli Twoj ruch to kamien, przegrywasz!')
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+else if(randomNumber ==2){
+  computerMove = 'papier'
+}
+
+else if(randomNumber ==3){
+  computerMove = 'nozyce'
+}
+
+printMessage('Mój ruch to: ' + computerMove);
