0709ed60e7f32efb8e77f2d14fa03d25aaff4bcf
Grzegorz Siudak <siudak.grzegorz@gmail.com>
2021-11-28 18:33:02 +0000

    Correct code

--- js/script.js
+++ js/script.js
@@ -79,11 +79,11 @@ function displayResult(argComputerMove, argPlayerMove){
 
 
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+const randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = getMoveName(randomNumber);
+const computerMove = getMoveName(randomNumber);
 
 
 
@@ -94,7 +94,7 @@ printMessage('Ruch computera to: ' + computerMove);
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(playerInput);
+const playerMove = getMoveName(playerInput);
 
 
 printMessage('Ruch gracza to: ' + playerMove);
